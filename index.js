bdafa473321284c6089cccfd4b2173fb75e7c9a9
Chun Yi <8.5by11@gmail.com>
2022-01-11 22:55:24 -0500

    commit tree f2aa47d0e1c4b3c0d00c2fc80f71629f188bf43b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return ([drivers[0],drivers[1]])
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return ([drivers[2],drivers[3]])
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return integer * fare;
+    }
+}
+
+createFareMultiplier(4);
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverArg) {
+    return driverArg(drivers);
+}
